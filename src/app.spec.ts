151698e25e2ee9f957e3d83c692ae8bc27c4d880
Yaroslav Mudryi <mudryi435@gmail.com>
2023-04-24 19:17:12 +0300

    removed ecr list command from pipeline; removed unused test; fixed main test

--- src/app.spec.ts
+++ src/app.spec.ts
@@ -1,10 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppService } from '../src/app.service';
-import { AppController } from '../src/app.controller';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
-describe('AppController (e2e)', () => {
+describe('AppController', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
@@ -19,8 +19,8 @@ describe('AppController (e2e)', () => {
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
-      .expect(404)
-      .expect('Not found');
+      .get('/app/health-check')
+      .expect(200)
+      .expect('true');
   });
 });
